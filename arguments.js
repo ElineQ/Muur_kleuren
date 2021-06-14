f1893e836b9592e5d06dac38cd42d7f058f4a238
ElineQ <84079851+ElineQ@users.noreply.github.com>
2021-06-14 22:25:06 +0200

    Add files via upload

---
+++ arguments.js
@@ -0,0 +1,15 @@
+const wall = function (item) {
+    console.log("The wall has been painted red")
+};
+
+const left_bucket = "Yellow paint";
+const right_bucket = "blue paint";
+
+wall(left_bucket);
+wall(right_bucket);
+
+const color = function (Noord, zuid) {
+    console.log(Noord + zuid)
+};
+
+color(oranje, grijs)
